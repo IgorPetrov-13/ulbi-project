bc02128bec23872f0a225021bab0f7fad6f13640
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2025-01-09 22:27:34 +0300

    Routing and Lazy loading were added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import Counter from './components/Counter';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { Link } from 'react-router-dom';
+import AboutPageAsync from './pages/AboutPage/AboutPage.async';
+import MainPageAsync from './pages/MainPage/MainPage.async';
+import { Suspense } from 'react';
 
 function App() {
   return (
-    <div>
-      <Counter />
+    <div className="app">
+      <Link to={'/about'}>About</Link>
+      <Link to={'/'}>Main</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
