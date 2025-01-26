86e1c0eeab6613c021f66de5f4e45b4532c82a56
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2025-01-26 20:17:09 +0300

    Sidebar was added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,17 @@ import { classNames } from '../shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider/index';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 function App() {
   const { theme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className="page-content">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 }
