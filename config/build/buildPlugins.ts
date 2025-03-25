4867c28b8113c9f4b3eb3ba87a9a9b5040cfb267
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2025-03-25 20:05:34 +0300

    plugin fixed

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,8 +4,8 @@ import { BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
-export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -14,9 +14,11 @@ export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInst
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+  }
+  return plugins;
 }
